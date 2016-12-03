457e734c3e6b7f4ac47672b68e06409aac847fb2
ninglvfeihong <ninglvfeihong@163.com>
2016-12-03 20:45:05 +0000

    switch deep and deeper test

--- deep/src/main.c
+++ deep/src/main.c
@@ -1,6 +1,5 @@
 #include "../head/hellolib.h"
 #include <stdio.h>
-#include <config.h>
 int main(){
 	sayHello();
 	exit(0);
--- deeper/src/main.c
+++ deeper/src/main.c
@@ -1,5 +1,6 @@
 #include "../head/hellolib.h"
 #include <stdio.h>
+#include <config.h>
 int main(){
 	sayHello();
 	exit(0);
